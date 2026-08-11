cd2436219024c983408bc66ec8666f2a0b2387b6
Tara Skiba <tskiba@vols.utk.edu>
2026-08-11 11:29:34 -0400

    update name from skiba to forestspot

--- Home.py
+++ Home.py
@@ -16,16 +16,16 @@ logo = "https://github.com/taraskiba/skiba/blob/a98750c413bd869324c551e7910886b0
 st.sidebar.image(logo)
 
 # Customize page title
-st.title("Streamlit for the _skiba_ package")
+st.title("_forestspot_ on Streamlit")
 
 
-st.header("A streamlit app version of the skiba package")
+st.header("A streamlit app version of the forestspot package")
 
 markdown = """
-This app is based on the _skiba_ package and allows users to extract data from Google Earth Engine (GEE) using on a point or area scale.  This package pulls the average pixel value for a specified area for the entire GEE dataset or optional time period. Consult the GEE page for the dataset of interest to view the pixel size and time period. \n
-Due to some of the _skiba_ package's dependencies not being available in the Streamlit environment, this app is not built with the skiba package. 
-Instead, it is recreated in Streamlit to perform the same as queries from Google Earth Engine as done _skiba_. Many portions of the streamlit version has code pulled directly from the _skiba_ package, but modified to function within Streamlit.
-You can compare the _skiba_ package's GitHub page ([_skiba_ on GitHub](https://github.com/taraskiba/skiba.git)) with this app's GitHub page ([_streamlit-skiba_ on GitHub](https://github.com/taraskiba/streamlit-skiba)) for similarities and differences.\n
+This app is based on the _forestspot_ package and allows users to extract data from Google Earth Engine (GEE) using on a point or area scale.  This package pulls the average pixel value for a specified area for the entire GEE dataset or optional time period. Consult the GEE page for the dataset of interest to view the pixel size and time period. \n
+Due to some of the _forestspot_ package's dependencies not being available in the Streamlit environment, this app is not built with the _forestspot_ package. 
+Instead, it is recreated in Streamlit to perform the same as queries from Google Earth Engine as done _forestspot_. Many portions of the streamlit version has code pulled directly from the _skiba_ package, but modified to function within Streamlit.
+You can compare the _forestspot_ package's GitHub page ([_forestspot_ on GitHub](https://github.com/taraskiba/forestspot.git)) with this app's GitHub page ([_streamlit-skiba_ on GitHub](https://github.com/taraskiba/streamlit-skiba)) for similarities and differences.\n
 **Note:** carefully read over instructions for each section to ensure formatting and file types used are correct. \n
 
 *Please be considerate of your energy usage when using this app, as it is hosted on a server and uses Google Earth Engine's resources which contribute significant amounts of carbon. Please run only neccessary queries to minimize your carbon footprint.* \n
@@ -36,7 +36,7 @@ st.markdown("— Robert Swan ☯")
 
 
 markdown = """
-[tskiba@vols.utk.edu](mailto:tskiba@vols.utk.edu?subject=Skiba%20Package)
+[tskiba@vols.utk.edu](mailto:tskiba@vols.utk.edu?subject=forestspot%20Package)
 for any problems or questions. \n
 Google Earth Engine may need to be reauthenticated from time to time.
 """
--- pages/Tutorial.py
+++ pages/Tutorial.py
@@ -5,14 +5,14 @@ st.set_page_config(layout="wide")
 
 # Customize the sidebar
 markdown = """
-Web App for the Skiba package
+Web App for the forestspot package
 ========================
-<https://github.com/taraskiba/streamlit-skiba>
+<https://github.com/taraskiba/streamlit-forestspot>
 """
 
 st.sidebar.title("About")
 st.sidebar.info(markdown)
-logo = "https://github.com/taraskiba/skiba/blob/a98750c413bd869324c551e7910886b0cd2d2d77/docs/files/logo.png?raw=true"
+logo = "https://github.com/taraskiba/forestspot/blob/a98750c413bd869324c551e7910886b0cd2d2d77/docs/files/logo.png?raw=true"
 st.sidebar.image(logo)
 
 # Customize page title
@@ -20,7 +20,7 @@ st.title("User Guide")
 
 markdown = """
 Slides from September 17, 2025 SNR Seminar and FIA Presentation:
-[here](https://github.com/taraskiba/streamlit-skiba/blob/main/skiba_%20a%20forester%E2%80%99s%20package%20to%20retrieve%20Google%20Earth%20Engine%20data.pdf?raw=true)
+[here](https://github.com/taraskiba/streamlit-forestspot/blob/main/skiba_%20a%20forester%E2%80%99s%20package%20to%20retrieve%20Google%20Earth%20Engine%20data.pdf?raw=true)
 """
 
 st.markdown(markdown)
@@ -30,7 +30,7 @@ st.header("Walkthroughs and FAQs")
 st.subheader("Which tools are right for me?")
 
 markdown = """
-![Flowchart](https://github.com/taraskiba/skiba/blob/main/docs/files/flowchart.png?raw=true)
+![Flowchart](https://github.com/taraskiba/forestspot/blob/main/docs/files/flowchart.png?raw=true)
 """
 
 st.markdown(markdown)
@@ -51,7 +51,7 @@ st.subheader("Determine your buffer size")
 markdown = """
 If you are dealing with sensitive data, you may want to use a buffer around your points to avoid exact locations being used. The radius of the buffer size should take into account the radius of you inventory plot and the pixel size of the GEE dataset you are using.
 The following figure illustrates some common cases of how points can be buffered, given various plot and pixel sizes.
-![Buffered points](https://github.com/taraskiba/skiba/blob/main/docs/files/buffered_diagram.png?raw=true)
+![Buffered points](https://github.com/taraskiba/forestspot/blob/main/docs/files/buffered_diagram.png?raw=true)
 Please refer to the upcoming publication and your data's user guide on handling confidential information for more information.
 """
 st.markdown(markdown)
